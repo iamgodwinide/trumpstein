f07a5b86188fbc1c72bf157e0118b0227c889710
Godwin Idemudia <godwinide@gmail.com>
2024-06-05 18:51:46 +0100

    new

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 const enter = document.querySelector("#enter");
 const audio = document.getElementById("audio");
 
-enter.addEventListener("click", () => {
-    console.log(audio)
-    audio.play();
-    document.querySelector(".overlay").style.display = "none";
-})
+if (enter) {
+    enter.addEventListener("click", () => {
+        console.log(audio)
+        audio.play();
+        document.querySelector(".overlay").style.display = "none";
+    })
+}
 
 
 const styleBoxes = () => {
@@ -15,17 +17,20 @@ const styleBoxes = () => {
 
         const area = document.getElementById(`area${i + 1}`);
 
-        const coords = area.coords.split(",");
+        if (area) {
+            const coords = area.coords.split(",");
 
-        box.style.width = (Math.abs(coords[0] - coords[2])) + "px"
-        box.style.height = (Math.abs(coords[1] - coords[3])) + "px"
-        box.style.left = Number(coords[0]) + "px"
-        box.style.top = Number(coords[1]) + "px"
+            box.style.width = (Math.abs(coords[0] - coords[2])) + "px"
+            box.style.height = (Math.abs(coords[1] - coords[3])) + "px"
+            box.style.left = Number(coords[0]) + "px"
+            box.style.top = Number(coords[1]) + "px"
+        }
     });
 
 }
 
 setInterval(() => {
     styleBoxes();
-}, 1000)
+}, 1000);
+
 
